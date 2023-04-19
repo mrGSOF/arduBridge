7ce22997c874588de745f23616a0e7ac055419dd
Guy Soffer (GSOF) <gsoffer@yahoo.com>
2023-04-18 21:39:50 -0700

    Add quick help to code example.

--- Examples/ArduBridge_IDE.py
+++ Examples/ArduBridge_IDE.py
@@ -5,13 +5,23 @@ To customize the environment to your needs. You will need to change
 he parameters in the "PARAMETER BLOCK" in the __main__ section
 
 By: Guy Soffer
-Date: 21/Dec/2021
+Date: 18/Apr/2023
 """
 
 #Basic modules to load
 from GSOF_ArduBridge import ArduBridge
 from GSOF_ArduBridge import ArduBridge_HW
 
+def quickHelp():
+    print("Quick help:")
+    print("ardu.gpio.pinMode(pin=1, mode=ardu.gpio.INPUT)")
+    print("ardu.gpio.pinMode(pin=2, mode=ardu.gpio.OUTPUT)")
+    print("ardu.gpio.digitalRead(1)")
+    print("ardu.gpio.digitalWrite(pin=2, val=1)")
+    print("ardu.gpio.pinPulse(pin=2, onTime=2)")
+    print("ardu.analogWrite(pin=3, val=128)")
+    print("ardu.analogRead(pin=1)")
+    
 if __name__ == "__main__":
     #\/\/\/ CHANGE THESE PARAMETERS \/\/\/
     port = "COM6"          #< Change to the correct COM to access the Arduino
@@ -21,16 +31,17 @@ if __name__ == "__main__":
     
     print('Using port %s at %d'%(port, baudRate))
     ardu = ArduBridge.ArduBridge( COM=port, baud=baudRate )
-    ards = ArduBridge.ArduBridge( ardu, an_ref=3.3 )
+    ards = ArduBridge_HW.ArduBridge_Shield( ardu, an_ref=3.3 )
 
     print('Discovering ArduBridge on port %s'%(port))
     if ardu.OpenClosePort(1):
         print('ArduBridge is ON-LINE.')
     else:
         print('ArduBridge is not responding.')
+    quickHelp()
 
     #\/\/\/ REST OF YOUR CODE GOES HERE \/\/\/
-
+    
     #/\/\/\        CODE BLOCK END       /\/\/\
 
 
