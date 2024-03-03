235b7c392e930d0fb0656c04f2eb5c2956e10897
Guy Soffer <gsoffer@yahoo.com>
2024-03-02 21:36:15 -0800

    Rearranged the stack boards to pins to be linear.

--- GSOF_ArduBridge/ElectrodeGpioStack.py
+++ GSOF_ArduBridge/ElectrodeGpioStack.py
@@ -45,7 +45,7 @@ class ExtGpioStack():
         self.i2c = i2c
         self.devList = devList
         if len(self.devList) == 0:
-            self.devList = range(0x40, 0x46, 1)
+            self.devList = range(0x40, 0x48, 1)
         self.pin2pin = pin2pin
         if self.pin2pin == False:
             self.pin2pin = range(0,20,1)
@@ -56,13 +56,13 @@ class ExtGpioStack():
             print('\nConfiguring port-extenderID 0x%02x'%(dev))
             if self.i2c:
                 self.ExtGpio.append( Max3700ExtGPIO.Max3700ExtGPIO( i2c=self.i2c, devID=dev, v=self.v ) )
-                self.ExtGpio[-1].modeSet(mode=1) #Activating the MAX3700
-                self.ExtGpio[-1].modeGet()       #Readback the MAX3700 mode
+                self.ExtGpio[-1].modeSet(mode=1)             #< Activating the MAX3700
+                self.ExtGpio[-1].modeGet()                   #< Readback the MAX3700 mode
                 self.ExtGpio[-1].bankModeGet()
-                self.ExtGpio[-1].bankModeSet(0x55, B=0, N=7) #Setting all of its ports to output
-                self.ExtGpio[-1].bankModeGet()               #Reading back the written data
+                self.ExtGpio[-1].bankModeSet(0x55, B=0, N=7) #< Setting all of its ports to output
+                self.ExtGpio[-1].bankModeGet()               #< Reading back the written data
                 for pin in [0,8,16,24]:
-                    self.ExtGpio[-1].portWrite(pin, 0x00)    #Simultaniously write 0 to 8 pins
+                    self.ExtGpio[-1].portWrite(pin, 0x00)    #< Simultaniously write 0 to 8 pins
 
             else:
                 print('No I2C object...')
@@ -129,27 +129,35 @@ class ExtGpioStack():
     def pin2dev( self, electrode_number ):
         """Each device has 20-IO pins"""
         if electrode_number < 20:
-            if len(self.ExtGpio) > 2:
-                return self.ExtGpio[2]
-
-        if (electrode_number >= 20) and (electrode_number < 40):
-            if len(self.ExtGpio) > 3:
-                return self.ExtGpio[3]
-
-        if (electrode_number >= 40) and (electrode_number < 60): 
-            if len(self.ExtGpio) > 4:
-                return self.ExtGpio[4]
-
-        if (electrode_number >= 60) and (electrode_number < 80):
-            if len(self.ExtGpio) > 5:
-                return self.ExtGpio[5]
-
-        if (electrode_number >= 80) and (electrode_number < 100):
             if len(self.ExtGpio) > 0:
                 return self.ExtGpio[0]
 
-        if (electrode_number >= 100) and (electrode_number < 120):
+        if (electrode_number >= 20) and (electrode_number < 40):
             if len(self.ExtGpio) > 1:
                 return self.ExtGpio[1]
+
+        if (electrode_number >= 40) and (electrode_number < 60): 
+            if len(self.ExtGpio) > 2:
+                return self.ExtGpio[2]
+
+        if (electrode_number >= 60) and (electrode_number < 80):
+            if len(self.ExtGpio) > 3:
+                return self.ExtGpio[3]
+
+        if (electrode_number >= 80) and (electrode_number < 100):
+            if len(self.ExtGpio) > 4:
+                return self.ExtGpio[4]
+
+        if (electrode_number >= 100) and (electrode_number < 120):
+            if len(self.ExtGpio) > 5:
+                return self.ExtGpio[5]
+
+        if (electrode_number >= 120) and (electrode_number < 140):
+            if len(self.ExtGpio) > 6:
+                return self.ExtGpio[6]
+
+        if (electrode_number >= 140) and (electrode_number < 160):
+            if len(self.ExtGpio) > 7:
+                return self.ExtGpio[7]
 
         return -1
